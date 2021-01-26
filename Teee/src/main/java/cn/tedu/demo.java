e8336208efce043518881e44cec268f09a58f7da
Admin <1819943902@qq.com>
2021-01-26 22:27:35 +0800

    试试

---
+++ Teee/src/main/java/cn/tedu/demo.java
@@ -0,0 +1,13 @@
+package cn.tedu;
+
+/**
+ *@BelongsProject:Teee
+ *@BelongsPackage:cn.tedu
+ *@Author:Admin
+ *@Description:
+ */
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("试试！！！");
+    }
+}
